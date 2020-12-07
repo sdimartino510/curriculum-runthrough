a4686d7da436fc6b741eca4154df6457516d01cf
Sergio Di Martino <sergio@webdevserg.io>
2020-12-07 12:32:42 -0800

    initial commit

---
+++ 06-Server-Side-APIs/02-Homework/Main/script.js
@@ -0,0 +1,173 @@
+function getSearchVal() {
+  var searchValue = document.querySelector("#search-value").value;
+  searchWeather(searchValue);
+  makeRow(searchValue);
+}
+
+function makeRow(searchValue) {
+  var liEl = document.createElement("li");
+  liEl.classList.add("list-group-item", "list-group-item-action");
+  var text = searchValue;
+  liEl.textContent = text;
+  var historyEl = document.querySelector(".history");
+  console.log(event.target);
+  historyEl.onclick = function () {
+    console.log(event.target.tagName);
+    if (event.target.tagName == "LI") {
+      searchWeather(event.target.textContent);
+    }
+  };
+  historyEl.appendChild(liEl);
+}
+
+function searchWeather(searchValue) {
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      searchValue +
+      "&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // clear any old content
+      todayEl = document.querySelector("#today");
+      todayEl.textContent = " ";
+
+      // create html content for current weather'
+      var titleEl = document.createElement("h3");
+      titleEl.classList.add("card-title");
+      titleEl.textContent =
+        data.name + " (" + new Date().toLocaleDateString() + ")";
+      var cardEl = document.createElement("div");
+      cardEl.classList.add("card");
+      var windEl = document.createElement("p");
+      windEl.classList.add("card-text");
+      var humidEl = document.createElement("p");
+      humidEl.classList.add("card-text");
+      var tempEl = document.createElement("p");
+      tempEl.classList.add("card-text");
+      humidEl.textContent = "Humidity: " + data.main.humidity + " %";
+      tempEl.textContent = "Temperature: " + data.main.temp + " °F";
+      var cardBodyEl = document.createElement("div");
+      cardBodyEl.classList.add("card-body");
+      var imgEl = document.createElement("img");
+      imgEl.setAttribute(
+        "src",
+        "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+      );
+
+      titleEl.appendChild(imgEl);
+      cardBodyEl.appendChild(titleEl);
+      cardBodyEl.appendChild(tempEl);
+      cardBodyEl.appendChild(humidEl);
+      cardBodyEl.appendChild(windEl);
+      cardEl.appendChild(cardBodyEl);
+      todayEl.appendChild(cardEl);
+
+      getForecast(searchValue);
+      getUVIndex(data.coord.lat, data.coord.lon);
+    });
+}
+
+function getForecast(searchValue) {
+  fetch(
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+      searchValue +
+      "&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var forecastEl = document.querySelector("#forecast");
+      forecastEl.innerHTML = '<h4 class="mt-3">5-Day Forecast:</h4>';
+      forecastRowEl = document.createElement("div");
+      forecastRowEl.className = '"row"';
+
+      // loop over all forecasts (by 3-hour increments)
+      for (var i = 0; i < data.list.length; i++) {
+        // only look at forecasts around 3:00pm
+        if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+          // create html elements for a bootstrap card
+          var colEl = document.createElement("div");
+          colEl.classList.add("col-md-2");
+          var cardEl = document.createElement("div");
+          cardEl.classList.add("card", "bg-primary", "text-white");
+          var windEl = document.createElement("p");
+          windEl.classList.add("card-text");
+          windEl.textContent =
+            "Wind Speed: " + data.list[i].wind.speed + " MPH";
+          var humidityEl = document.createElement("p");
+          humidityEl.classList.add("card-text");
+          humidityEl.textContent =
+            "Humidity : " + data.list[i].main.humidity + " %";
+          var bodyEl = document.createElement("div");
+          bodyEl.classList.add("card-body", "p-2");
+          var titleEl = document.createElement("h5");
+          titleEl.classList.add("card-title");
+          titleEl.textContent = new Date(
+            data.list[i].dt_txt
+          ).toLocaleDateString();
+          var imgEl = document.createElement("img");
+          imgEl.setAttribute(
+            "src",
+            "http://openweathermap.org/img/w/" +
+              data.list[i].weather[0].icon +
+              ".png"
+          );
+          var p1El = document.createElement("p");
+          p1El.classList.add("card-text");
+          p1El.textContent = "Temp: " + data.list[i].main.temp_max + " °F";
+          var p2El = document.createElement("p");
+          p2El.classList.add("card-text");
+          p2El.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+
+          // merge together and put on page
+          colEl.appendChild(cardEl);
+          bodyEl.appendChild(titleEl);
+          bodyEl.appendChild(imgEl);
+          bodyEl.appendChild(windEl);
+          bodyEl.appendChild(humidityEl);
+          bodyEl.appendChild(p1El);
+          bodyEl.appendChild(p2El);
+          cardEl.appendChild(bodyEl);
+          forecastEl.appendChild(colEl);
+        }
+      }
+    });
+}
+
+function getUVIndex(lat, lon) {
+  fetch(
+    "http://api.openweathermap.org/data/2.5/uvi?appid=d91f911bcf2c0f925fb6535547a5ddc9&lat=" +
+      lat +
+      "&lon=" +
+      lon
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var bodyEl = document.querySelector(".card-body");
+      var uvEl = document.createElement("p");
+      uvEl.textContent = "UV Index: ";
+      var buttonEl = document.createElement("span");
+      buttonEl.classList.add("btn", "btn-sm");
+      buttonEl.innerHTML = data.value;
+
+      if (data.value < 3) {
+        buttonEl.classList.add("btn-success");
+      } else if (data.value < 7) {
+        buttonEl.classList.add("btn-warning");
+      } else {
+        buttonEl.classList.add("btn-danger");
+      }
+
+      bodyEl.appendChild(uvEl);
+      uvEl.appendChild(buttonEl);
+    });
+}
+
+document.querySelector("#search-button").addEventListener("click", getSearchVal);
---
+++ 06-Server-Side-APIs/README.md
@@ -0,0 +1,100 @@
+# Unit 06: Server-Side APIs
+
+## Overview:
+
+In a full-stack application, the client (i.e., the front end) makes requests to the server (i.e., the back end) to get data to display to the user. One way the client can request this data is by using the Fetch Web API. The Fetch API can even request data from third-party APIs, like the OpenWeather API.
+
+In this unit, we'll use the `fetch()` method to make requests, parse through the returned datasets, and convert them to HTML elements. We'll explore how this data is typically formatted, how to look for and handle errors that might arise, and more importantly, how to read documentation when working with server-side APIs.
+
+## Key Topics
+
+We'll cover the following topics in this unit:
+
+* Client-server model
+
+* JSON format
+
+* URLs
+
+* HTTP GET requests
+
+* HTTP status codes
+
+* Fetch API
+
+* `curl`
+
+* DevTools Network tab
+
+* `document.location`
+
+## Learning Objectives
+
+You will be employer-ready if you are able to:
+
+* Explain what JSON is.
+
+* Explain the methods that can be used to request data from the server.
+
+* Explain the difference between a client-side and server-side API.
+
+* Explain the client-server model and request-response pattern.
+
+* Implement and explain the differences between making HTTP GET requests with XMLHttpRequest, jQuery AJAX, and the Fetch API.
+
+* Explain HTTP response codes and handle response metadata with the Fetch API.
+
+* Parse JSON to dynamically generate HTML.
+
+* Explain the benefits and challenges of working with asynchronous JavaScript.
+
+* Explain and implement URL query string parameters.
+
+## Technical Interview Preparation
+
+You will be employer-competitive if you are able to solve the following algorithms and successfully complete the assessments.
+
+### Algorithms
+
+* [01: Is Palindrome](./03-Algorithms/01-is-palindrome)
+
+* [02: Factorial](./03-Algorithms/02-factorial)
+
+  * [📹 Let's Code Factorial!](https://2u-20.wistia.com/medias/gnyfobes5c)
+
+* [03: Title Case](./03-Algorithms/03-title-case)
+
+### Assessments
+
+Assess your knowledge by answering technical interview questions and solving coding challenges.
+
+* [Unit 06 Assessment](https://forms.gle/xcqMRiysBemJpvz78)
+
+## Homework
+
+For your homework, you'll build a weather dashboard using various endpoints from the OpenWeather API.
+
+## Career Connection
+
+Career Services material for this unit is located in the [Career Connection folder](../../../01-Class-Content/{06-Server-Side-APIs}/04-Career-Connection/README.md). For more information about Career Services, including coding milestones, demo days, technical toolkits, workshops, and additional resources, visit the [Career Services website](http://bit.ly/CodingCS).
+
+## Heads-Up
+
+Get ready for your first group project! In the next unit, you and your teammates will use all of your new skills to build an app of your choosing.
+
+## Resources
+
+Here are some additional resources to help solidify the topics covered in this unit:
+
+* [MDN Web Docs on using the Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
+
+* [MDN Web Docs on Location](https://developer.mozilla.org/en-US/docs/Web/API/Location)
+
+* [Chrome DevTools tutorial on inspecting network activity](https://developers.google.com/web/tools/chrome-devtools/network)
+
+* [GitHub API documentation](https://docs.github.com/en/rest/reference)
+
+* [OpenWeather API documentation](https://openweathermap.org/api)
+
+---
+© 2020 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
